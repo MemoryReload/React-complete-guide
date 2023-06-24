0f1d68072cbefd0ea7b06928fdc84caee3429563
MemoryReload <heping_tsdwx@163.com>
2023-06-24 21:54:33 +0800

    add a card style component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,16 @@
 import React from "react";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css"
+import Card from "./Card";
 
 export default function ExpenseItem({ date, amount, desc }) {
     return (
-        <div className="expense-item">
+        <Card className="expense-item">
             <ExpenseDate date={date} />
             <div className="expense-item__description">
                 <h2>{desc}</h2>
                 <div className="expense-item__price">{amount}</div>
             </div>
-        </div>
+        </Card>
     );
 }
--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import Card from './Card';
 import ExpenseItem from './ExpenseItem';
 
 import "./ExpenseList.css"
 
 export default function ExpneseList({ expenses }) {
     return (
-        <div className='expenses'>
+        <Card className='expenses'>
             <ExpenseItem date={expenses[0].date} desc={expenses[0].title} amount={expenses[0].amount} />
             <ExpenseItem date={expenses[1].date} desc={expenses[1].title} amount={expenses[1].amount} />
             <ExpenseItem date={expenses[2].date} desc={expenses[2].title} amount={expenses[2].amount} />
-        </div>
+        </Card>
     );
 }
